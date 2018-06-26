ae8f18c5a747b63667b0fa24120c1ab3d3370a46
Alex Gyulbudaghyan <alex.gulbudaghyan@gmail.com>
2018-06-26 13:29:19 +0400

    [Special offer section]

--- wp-content/themes/lasiktheme/home.php
+++ wp-content/themes/lasiktheme/home.php
@@ -129,21 +129,27 @@ get_header(); ?>
   </div>
   <!--=== Top subsection end ===-->
   <!--=== Special Offer start ===-->
-  <div class="special-offer">
-    <div class="container align-center text-white">
-      <h2 class="special-offer__title">Special Offer</h2>
-      <h3 class=special-offer__subtitle>
-        LASIK Starting as low as $250
-      </h3>
-      <p class="special-offer__text">
-        Hurry! Limited Time Offer!
-      </p>
-      <button class="btn-simple btn-simple--icon btn-simple--dark-green btn-simple--big special-offer__button waves-effect waves-light" type="button">
-        <i class="far fa-calendar-alt btn-simple__icon"></i>
-        <span class="btn-simple__text">Schedule Today!</span>
-      </button>
-    </div>
-  </div>
+  <?php  if( have_rows( 'top_section' ) ): ?>
+     <?php while( have_rows( 'top_section' ) ) : the_row(); ?>
+        <?php $special_offer_row = get_sub_field( 'special_offer' ); ?>    
+      <div class="special-offer">
+        <div class="container align-center text-white">          
+          <h2 class="special-offer__title"><?php echo $special_offer_row[ 'title' ];?></h2>
+          <h3 class=special-offer__subtitle>
+            <?php echo $special_offer_row[ 'subtitle' ]; ?>
+          </h3>
+          <p class="special-offer__text">
+            <?php echo $special_offer_row[ 'offer_text' ]; ?>
+          </p>
+          <button class="btn-simple btn-simple--icon btn-simple--dark-green btn-simple--big special-offer__button waves-effect waves-light" type="button">
+            <i class="far fa-calendar-alt btn-simple__icon"></i>
+            <span class="btn-simple__text"><?php echo $special_offer_row[ 'schedule_button_text' ]; ?></span>
+          </button>
+
+      <?php endwhile; ?>
+        </div>
+      </div>
+  <?php endif; ?>
   <!--=== Special Offer end ===-->
 </section>
 <!--=== Top section end ===-->
