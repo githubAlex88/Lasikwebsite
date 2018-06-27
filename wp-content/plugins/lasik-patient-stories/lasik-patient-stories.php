c5e318557bd911f9938d08d4655769883eca974b
Alex Gyulbudaghyan <alex.gulbudaghyan@gmail.com>
2018-06-27 16:34:59 +0400

    [Refactor patient stories plugin, refactor stories query]

--- wp-content/plugins/lasik-patient-stories/lasik-patient-stories.php
+++ wp-content/plugins/lasik-patient-stories/lasik-patient-stories.php
@@ -57,156 +57,254 @@
 
 
 
-function create_story_post_type() {
-    register_post_type( 'story',
-        array(
-            'labels'       => array(
-                'name'       => __( 'Stories' ),
-                'add_new_item'       => __('Add New Story'),
-                'edit_item'          => __('Edit Story'),
-                'new_item'           => __('New Story'),
-                'view_item'          => __('View Story'),
-                'search_items'       => __('Search Story'),
-                'not_found'          => __('No story found.'),
-                'not_found_in_trash' => __('No story found in trash.'),
-                'all_items'          => __('All Stories'),
-                'menu_name'          => __('Story'),
-            ),
-            'public'       => true,
-            'hierarchical' => true,
-            'has_archive'  => true,
-            'supports'     => array(
-                'title',
-                'editor',
-                'excerpt',
-                'thumbnail',
-            ), 
-            'taxonomies'   => array(
-                'post_tag',
-                'category',
-            )
-        )
-    );
-    register_taxonomy_for_object_type( 'category', 'story' );
-    register_taxonomy_for_object_type( 'post_tag', 'story' );
-}
-add_action( 'init', 'create_story_post_type' );
+// function create_story_post_type() {
+//     register_post_type( 'story',
+//         array(
+//             'labels'       => array(
+//                 'name'       => __( 'Stories' ),
+//                 'add_new_item'       => __('Add New Story'),
+//                 'edit_item'          => __('Edit Story'),
+//                 'new_item'           => __('New Story'),
+//                 'view_item'          => __('View Story'),
+//                 'search_items'       => __('Search Story'),
+//                 'not_found'          => __('No story found.'),
+//                 'not_found_in_trash' => __('No story found in trash.'),
+//                 'all_items'          => __('All Stories'),
+//                 'menu_name'          => __('Story'),
+//             ),
+//             'public'       => true,
+//             'hierarchical' => true,
+//             'has_archive'  => true,
+//             'supports'     => array(
+//                 'title',
+//                 'editor',
+//                 'excerpt',
+//                 'thumbnail',
+//             ), 
+//             'taxonomies'   => array(
+//                 'post_tag',
+//                 'category',
+//             )
+//         )
+//     );
+//     register_taxonomy_for_object_type( 'category', 'story' );
+//     register_taxonomy_for_object_type( 'post_tag', 'story' );
+// }
+// add_action( 'init', 'create_story_post_type' );
 
-function add_story_meta_boxes() {
-    add_meta_box(
-        'story_fields_meta_boxes', // $id
-        'Story Fields', // $title
-        'show_story_fields_meta_boxes', // $callback
-        'story', // $screen
-        'normal', // $context
-        'high' // $priority
-    );
-}
-add_action( 'add_meta_boxes', 'add_story_meta_boxes' );
+    function create_story_cpt() {
 
-function show_story_fields_meta_boxes() {
-    global $post;  
-    
-        $meta = get_post_meta( $post->ID, 'story_fields', true ); ?>
+      $labels = array(
+        'name' => __( 'Stories', 'Post Type General Name', 'textdomain' ),
+        'singular_name' => __( 'Story', 'Post Type Singular Name', 'textdomain' ),
+        'menu_name' => __( 'Stories', 'textdomain' ),
+        'name_admin_bar' => __( 'Story', 'textdomain' ),
+        'archives' => __( 'Story Archives', 'textdomain' ),
+        'attributes' => __( 'Story Attributes', 'textdomain' ),
+        'parent_item_colon' => __( 'Parent Story:', 'textdomain' ),
+        'all_items' => __( 'All Stories', 'textdomain' ),
+        'add_new_item' => __( 'Add New Story', 'textdomain' ),
+        'add_new' => __( 'Add New', 'textdomain' ),
+        'new_item' => __( 'New Story', 'textdomain' ),
+        'edit_item' => __( 'Edit Story', 'textdomain' ),
+        'update_item' => __( 'Update Story', 'textdomain' ),
+        'view_item' => __( 'View Story', 'textdomain' ),
+        'view_items' => __( 'View Stories', 'textdomain' ),
+        'search_items' => __( 'Search Story', 'textdomain' ),
+        'not_found' => __( 'Not found', 'textdomain' ),
+        'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+        'featured_image' => __( 'Featured Image', 'textdomain' ),
+        'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+        'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+        'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+        'insert_into_item' => __( 'Insert into Story', 'textdomain' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this Story', 'textdomain' ),
+        'items_list' => __( 'Stories list', 'textdomain' ),
+        'items_list_navigation' => __( 'Stories list navigation', 'textdomain' ),
+        'filter_items_list' => __( 'Filter Stories list', 'textdomain' ),
+      );
+      $args = array(
+        'label' => __( 'Story', 'textdomain' ),
+        'description' => __( 'Patient Story', 'textdomain' ),
+        'labels' => $labels,
+        'menu_icon' => 'dashicons-admin-post',
+        'supports' => array('title', 'editor', 'thumbnail', ),
+        'taxonomies' => array(),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'hierarchical' => false,
+        'exclude_from_search' => false,
+        'show_in_rest' => true,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+      );
+      register_post_type( 'story', $args );
 
-  <input type="hidden" name="your_meta_box_nonce" value="<?php echo wp_create_nonce( basename(__FILE__) ); ?>">
-
-  <p>
-    <label for="story_fields[title]">Story Title</label>
-    <br>
-    <input type="text" name="story_fields[title]" id="story_fields[title]" class="regular-title" value="<?php if (is_array($meta) && isset($meta['title'])) { echo $meta['title']; } ?>">
-  </p>
-    <p>
-    <label for="story_fields[subtitle]">Story Subtitle</label>
-    <br>
-    <input type="text" name="story_fields[subtitle]" id="story_fields[subtitle]" class="regular-subtitle" value="<?php if (is_array($meta) && isset($meta['subtitle'])) { echo $meta['subtitle']; } ?>">
-  </p>
-  <p>
-    <label for="story_fields[description]">Story Description</label>
-    <br>
-    <textarea name="story_fields[description]" id="story_fields[description]" rows="5" cols="30" style="width:500px;"><?php if( isset($meta['description']) )echo $meta['description']; ?></textarea>
-  </p>
-  <p>
-    <label for="story_fields[is_visible_home]">Visible on home page
-            <input type="checkbox" name="story_fields[is_visible_home]" value="is_visible_home" <?php if ( isset($meta['is_visible_home']) && $meta['is_visible_home'] === 'is_visible_home' ) echo 'checked'; ?>>
-        </label>
-  </p>
-  <p>
-    <label for="story_fields[image]">Patient Image</label><br>
-    <input type="text" name="story_fields[image]" id="story_fields[image]" class="meta-image regular-text" value="<?php if(isset($meta['image'])) echo $meta['image']; ?>">
-    <input type="button" class="button image-upload" value="Browse">
-  </p>
-  <div class="image-preview"><img src="<?php if(isset($meta['image'])) echo $meta['image']; ?>" style="max-width: 250px;"></div>
-
-
-  <script>
-    jQuery(document).ready(function ($) {
-      // Instantiates the variable that holds the media library frame.
-      var meta_image_frame;
-      // Runs when the image button is clicked.
-      $('.image-upload').click(function (e) {
-        // Get preview pane
-        var meta_image_preview = $(this).parent().parent().children('.image-preview');
-        // Prevents the default action from occuring.
-        e.preventDefault();
-        var meta_image = $(this).parent().children('.meta-image');
-        // If the frame already exists, re-open it.
-        if (meta_image_frame) {
-          meta_image_frame.open();
-          return;
-        }
-        // Sets up the media library frame
-        meta_image_frame = wp.media.frames.meta_image_frame = wp.media({
-          title: meta_image.title,
-          button: {
-            text: meta_image.button
-          }
-        });
-        // Runs when an image is selected.
-        meta_image_frame.on('select', function () {
-          // Grabs the attachment selection and creates a JSON representation of the model.
-          var media_attachment = meta_image_frame.state().get('selection').first().toJSON();
-          // Sends the attachment URL to our custom image input field.
-          meta_image.val(media_attachment.url);
-          meta_image_preview.children('img').attr('src', media_attachment.url);
-        });
-        // Opens the media library frame.
-        meta_image_frame.open();
-      });
-    });
-  </script>
-
-  <?php }
-function save_story_fields_meta( $post_id ) {   
-    // verify nonce
-    if ( isset($_POST['your_meta_box_nonce']) 
-            && !wp_verify_nonce( $_POST['your_meta_box_nonce'], basename(__FILE__) ) ) {
-            return $post_id; 
-        }
-    // check autosave
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-        return $post_id;
     }
-    // check permissions
-    if (isset($_POST['post_type'])) { //Fix 2
-        if ( 'page' === $_POST['post_type'] ) {
-            if ( !current_user_can( 'edit_page', $post_id ) ) {
-                return $post_id;
-            } elseif ( !current_user_can( 'edit_post', $post_id ) ) {
-                return $post_id;
-            }  
-        }
+    add_action( 'init', 'create_story_cpt', 0 );
+
+class storyfieldMetabox {
+  private $screen = array(
+    'Story',
+  );
+  private $meta_fields = array(
+    array(
+      'label' => 'Title',
+      'id' => 'title',
+      'type' => 'text',
+    ),
+    array(
+      'label' => 'Subtitle',
+      'id' => 'subtitle',
+      'type' => 'text',
+    ),
+    array(
+      'label' => 'Description',
+      'id' => 'description',
+      'type' => 'textarea',
+    ),
+    array(
+      'label' => 'Image',
+      'id' => 'image',
+      'type' => 'media',
+    ),
+    array(
+      'label' => 'Homepage Visibility',
+      'id' => 'is_homepage_visible',
+      'type' => 'checkbox',
+    ),
+  );
+  public function __construct() {
+    add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+    add_action( 'admin_footer', array( $this, 'media_fields' ) );
+    add_action( 'save_post', array( $this, 'save_fields' ) );
+  }
+  public function add_meta_boxes() {
+    foreach ( $this->screen as $single_screen ) {
+      add_meta_box(
+        'storyfield',
+        __( 'Story Field', 'textdomain' ),
+        array( $this, 'meta_box_callback' ),
+        $single_screen,
+        'advanced',
+        'default'
+      );
     }
-    
-    $old = get_post_meta( $post_id, 'story_fields', true );
-        if (isset($_POST['story_fields'])) { //Fix 3
-            $new = $_POST['story_fields'];
-            if ( $new && $new !== $old ) {
-                update_post_meta( $post_id, 'story_fields', $new );
-            } elseif ( '' === $new && $old ) {
-                delete_post_meta( $post_id, 'story_fields', $old );
+  }
+  public function meta_box_callback( $post ) {
+    wp_nonce_field( 'storyfield_data', 'storyfield_nonce' );
+    $this->field_generator( $post );
+  }
+  public function media_fields() {
+    ?><script>
+      jQuery(document).ready(function($){
+        if ( typeof wp.media !== 'undefined' ) {
+          var _custom_media = true,
+          _orig_send_attachment = wp.media.editor.send.attachment;
+          $('.storyfield-media').click(function(e) {
+            var send_attachment_bkp = wp.media.editor.send.attachment;
+            var button = $(this);
+            var id = button.attr('id').replace('_button', '');
+            _custom_media = true;
+              wp.media.editor.send.attachment = function(props, attachment){
+              if ( _custom_media ) {
+                $('input#'+id).val(attachment.url);
+              } else {
+                return _orig_send_attachment.apply( this, [props, attachment] );
+              };
             }
+            wp.media.editor.open(button);
+            return false;
+          });
+          $('.add_media').on('click', function(){
+            _custom_media = false;
+          });
         }
+      });
+    </script><?php
+  }
+  public function field_generator( $post ) {
+    $output = '';
+    foreach ( $this->meta_fields as $meta_field ) {
+      $label = '<label for="' . $meta_field['id'] . '">' . $meta_field['label'] . '</label>';
+      $meta_value = get_post_meta( $post->ID, $meta_field['id'], true );
+      if ( empty( $meta_value ) ) {
+        $meta_value = $meta_field['default']; }
+      switch ( $meta_field['type'] ) {
+        case 'media':
+          $input = sprintf(
+            '<input style="width: 80%%" id="%s" name="%s" type="text" value="%s"> <input style="width: 19%%" class="button storyfield-media" id="%s_button" name="%s_button" type="button" value="Upload" />',
+            $meta_field['id'],
+            $meta_field['id'],
+            $meta_value,
+            $meta_field['id'],
+            $meta_field['id']
+          );
+          break;
+        case 'checkbox':
+          $input = sprintf(
+            '<input %s id=" % s" name="% s" type="checkbox" value="1">',
+            $meta_value === '1' ? 'checked' : '',
+            $meta_field['id'],
+            $meta_field['id']
+            );
+          break;
+        case 'textarea':
+          $input = sprintf(
+            '<textarea style="width: 100%%" id="%s" name="%s" rows="5">%s</textarea>',
+            $meta_field['id'],
+            $meta_field['id'],
+            $meta_value
+          );
+          break;
+        default:
+          $input = sprintf(
+            '<input %s id="%s" name="%s" type="%s" value="%s">',
+            $meta_field['type'] !== 'color' ? 'style="width: 100%"' : '',
+            $meta_field['id'],
+            $meta_field['id'],
+            $meta_field['type'],
+            $meta_value
+          );
+      }
+      $output .= $this->format_rows( $label, $input );
+    }
+    echo '<table class="form-table"><tbody>' . $output . '</tbody></table>';
+  }
+  public function format_rows( $label, $input ) {
+    return '<tr><th>'.$label.'</th><td>'.$input.'</td></tr>';
+  }
+  public function save_fields( $post_id ) {
+    if ( ! isset( $_POST['storyfield_nonce'] ) )
+      return $post_id;
+    $nonce = $_POST['storyfield_nonce'];
+    if ( !wp_verify_nonce( $nonce, 'storyfield_data' ) )
+      return $post_id;
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+      return $post_id;
+    foreach ( $this->meta_fields as $meta_field ) {
+      if ( isset( $_POST[ $meta_field['id'] ] ) ) {
+        switch ( $meta_field['type'] ) {
+          case 'email':
+            $_POST[ $meta_field['id'] ] = sanitize_email( $_POST[ $meta_field['id'] ] );
+            break;
+          case 'text':
+            $_POST[ $meta_field['id'] ] = sanitize_text_field( $_POST[ $meta_field['id'] ] );
+            break;
+        }
+        update_post_meta( $post_id, $meta_field['id'], $_POST[ $meta_field['id'] ] );
+      } else if ( $meta_field['type'] === 'checkbox' ) {
+        update_post_meta( $post_id, $meta_field['id'], '0' );
+      }
+    }
+  }
 }
-
-add_action( 'save_post', 'save_story_fields_meta' );
+if (class_exists('storyfieldMetabox')) {
+  new storyfieldMetabox;
+};
--- wp-content/themes/lasiktheme/home.php
+++ wp-content/themes/lasiktheme/home.php
@@ -158,7 +158,7 @@ get_header(); ?>
     $args = array(
         'post_type'     => 'story',
         'post_status'   => 'publish',
-        'meta_key'      => 'show_on_homepage',
+        'meta_key'      => 'is_homepage_visible',
         'meta_value'    => '1'
     );
     $stories_query = new WP_Query( $args );
@@ -168,23 +168,23 @@ get_header(); ?>
         <div class="patient-stories__slider">
             <?php if(have_posts()) : while($stories_query->have_posts()) : $stories_query->the_post(); ?>
             <div class="patient-stories__item">
-                <?php $meta_data = get_post_meta($post->ID, 'story_fields')[0]; ?>
+                <?php $meta_data = get_post_meta($post->ID); ?>
                 <div class="row">
                     <div class="col x12 m6 l5 xl6 img">
                         <?php if( isset($meta_data['image']) ) : ?>
                         <div class="phone">
                             <picture class="hide-on-small-only">
-                                <source media="(min-width: 540px)" data-srcset="<?php echo $meta_data['image']; ?>" />
-                                <img  alt="article" src="<?php echo $meta_data['image']; ?>">
+                                <source media="(min-width: 540px)" data-srcset="<?php echo $meta_data['image'][0]; ?>" />
+                                <img  alt="article" src="<?php echo $meta_data['image'][0]; ?>">
                                 <noscript>
-                                    <img  alt="article" src="<?php echo $meta_data['image']; ?>" />
+                                    <img  alt="article" src="<?php echo $meta_data['image'][0]; ?>" />
                                 </noscript>
                             </picture>
                             <picture class="hide-on-med-and-up">
-                                <source media="(min-width: 540px)" data-srcset="<?php echo $meta_data['image']; ?>" />
-                                    <img  alt="article" src="<?php echo $meta_data['image']; ?>">
+                                <source media="(min-width: 540px)" data-srcset="<?php echo $meta_data['image'][0]; ?>" />
+                                    <img  alt="article" src="<?php echo $meta_data['image'][0]; ?>">
                                 <noscript>
-                                    <img  alt="article" src="<?php echo $meta_data['image']; ?>" />
+                                    <img  alt="article" src="<?php echo $meta_data['image'][0]; ?>" />
                                 </noscript>
                             </picture>
                         </div>
@@ -212,13 +212,13 @@ get_header(); ?>
                                 <ul class="custom-dots"></ul>
                             </div>
                             <div class="patient-stories__title">
-                                <?php echo $meta_data['title']; ?>
+                                <?php echo $meta_data['title'][0]; ?>
                             </div>
                             <div class="patient-stories__subtitle">
-                                <?php echo $meta_data['subtitle']; ?>
+                                <?php echo $meta_data['subtitle'][0]; ?>
                             </div>
                             <div class="patient-stories__description">
-                                <?php echo $meta_data['description']; ?>
+                                <?php echo $meta_data['description'][0]; ?>
                             </div>
                             <a href="<?php echo get_post_permalink($story_id);?>" class="btn-simple btn-simple--icon btn-simple--orange btn-simple--big">
                                 <i class="far fa-angle-right btn-simple__icon"></i>
